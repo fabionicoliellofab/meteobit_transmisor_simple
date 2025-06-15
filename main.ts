670678d6607655937cc4e17265c7de3bb522c571
Fabio Nicoliello <32755862+fabionicoliellofab@users.noreply.github.com>
2025-06-14 22:53:54 -0300

    variable   Presión

--- main.ts
+++ main.ts
@@ -1,20 +1,31 @@
 let velocidad = 0
-let humedad = 0
-let temp = 0
-let presion = 0
+let Humedad = 0
+let Temp = 0
+let Presión = 0
+BME280.PowerOn()
 BMP280.Address(BMP280_I2C_ADDRESS.ADDR_0x76)
 radio.setGroup(1)
+datalogger.setColumnTitles(
+"Presion",
+"Temp",
+"Humedad"
+)
 loops.everyInterval(120000, function () {
-    presion = BME280.pressure(BME280_P.hPa)
-    radio.sendString("presion")
-    radio.sendNumber(presion)
-    temp = BME280.temperature(BME280_T.T_C)
-    radio.sendString("temp")
-    radio.sendNumber(temp)
-    humedad = BME280.humidity()
-    radio.sendString("humedad")
-    radio.sendNumber(humedad)
+    Presión = BME280.pressure(BME280_P.hPa)
+    radio.sendString("Presión")
+    radio.sendNumber(Presión)
+    Temp = BME280.temperature(BME280_T.T_C)
+    radio.sendString("Temp")
+    radio.sendNumber(Temp)
+    Humedad = BME280.humidity()
+    radio.sendString("Humedad")
+    radio.sendNumber(Humedad)
     velocidad = pins.analogReadPin(AnalogReadWritePin.P3)
     radio.sendString("velocidad")
     radio.sendNumber(velocidad)
+    datalogger.log(
+    datalogger.createCV("Presión", BME280.pressure(BME280_P.hPa)),
+    datalogger.createCV("Temp", BME280.temperature(BME280_T.T_C)),
+    datalogger.createCV("Humedad", BME280.humidity())
+    )
 })
